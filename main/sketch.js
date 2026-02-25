ecccad32b837c17412ff39a313acb35d862e5aa1
vecval24 <valentine.vecchi@heig-vd.ch>
2026-02-25 11:01:45 +0100

    Fichiers html et js

---
+++ main/sketch.js
@@ -0,0 +1,52 @@
+console.log("sketch");
+
+let serial; // variable to hold an instance of the serialport library
+let portName = "/dev/tty.usbmodem11201"; // fill in your serial port name here
+
+let width = window.innerWidth;
+let height = window.innerHeight;
+
+function setup() {
+  console.log("Setup sketch");
+  createCanvas(width, height);
+
+  // serial
+  serial = new p5.SerialPort(); // make a new instance of the serialport library
+  serial.on("list", printList); // set a callback function for the serialport list event
+  serial.on("connected", serverConnected); // callback for connecting to the server
+  serial.on("open", portOpen); // callback for the port opening
+  serial.on("data", serialEvent); // callback for when new data arrives
+  serial.on("error", serialError); // callback for errors
+  serial.on("close", portClose); // callback for the port closing
+
+  serial.list(); // list the serial ports
+  serial.open(portName); // open a serial port
+}
+
+// get the list of ports:
+function printList(portList) {
+  // portList is an array of serial port names
+  for (let i = 0; i < portList.length; i++) {
+    console.log(i + ": " + portList[i]);
+  }
+}
+
+function serverConnected() {
+  console.log("connected to server.");
+}
+
+function portOpen() {
+  console.log("the serial port is opened.");
+}
+
+function serialEvent() {}
+
+function serialError(err) {
+  console.log("Something went wrong with the serial port. " + err);
+}
+
+function portClose() {
+  console.log("The serial port is closed.");
+}
+
+function draw() {}
